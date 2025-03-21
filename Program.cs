4cb9984e62debc289e5fe1f647262937aa1cd74d
thomas-lavoie <202142181@edu.clg.qc.ca>
2025-03-21 13:31:30 -0400

    Premier menu et MongoDB Drive

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MongoDB.Driver;
+
+bool keep_going = true;
+
+while (keep_going)
+{
+    Console.WriteLine("Bibliotheque");
+    Console.WriteLine();
+    Console.WriteLine("   ------   ");
+    Console.WriteLine();
+    Console.WriteLine("Q: Quitter");
+
+    ConsoleKey key = Console.ReadKey().Key;
+    
+    switch (key)
+    {
+        case ConsoleKey.Q:
+            keep_going = false;
+            break;
+        default:
+            break;
+    }
+
+    Console.Clear();
+}
